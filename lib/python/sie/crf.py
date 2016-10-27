1ca7b7be73440e341cf24d856e53e2841d9cc9aa
Erwin Marsi <emarsi@idi.ntnu.no>
2016-10-27 11:05:46 +0200

    Script for CV exp with CRF

--- lib/python/sie/crf.py
+++ lib/python/sie/crf.py
@@ -1,6 +1,7 @@
 import json
 from glob import glob
-from os.path import join, basename, splitext
+from os.path import join, basename
+from os import makedirs
 
 from sie import ENTITIES
 from sie.feats import Features
@@ -11,17 +12,17 @@ def collect_crf_data(iob_dir, *feat_dirs):
     Collect the data to train/eval CRF classifier.
     Labels for entities are derived from IOB tags in the files in the iob_dir.
     Features are collected from the json files in one or more feat_dir.
-    Group names are derived from the filenames.
+    Filenames are the basenames are the iob files.
     """
     data = dict((label, list()) for label in ENTITIES)
     data['feats'] = []
-    data['groups'] = []
+    data['filenames'] = []
 
     for iob_fname in glob(join(iob_dir, '*.json')):
         text_iob = json.load(open(iob_fname))
 
-        feat_basename = basename(iob_fname)
-        feat_filenames = [join(dir, feat_basename)  for dir in feat_dirs]
+        filename = basename(iob_fname)
+        feat_filenames = [join(dir, filename)  for dir in feat_dirs]
         text_feat = Features.from_file(*feat_filenames)
         assert len(text_iob) == len(text_feat)
         data['feats'] += text_feat
@@ -29,7 +30,7 @@ def collect_crf_data(iob_dir, *feat_dirs):
         for label in ENTITIES:
             data[label] += _text_iob_tags(text_iob, label)
 
-        data['groups'] += len(text_iob) * [splitext(basename(iob_fname))[0]]
+        data['filenames'] += len(text_iob) * [filename]
 
     return data
 
@@ -40,3 +41,41 @@ def _text_iob_tags(text_iob, label):
 
 def _sent_iob_tags(sent_iob, label):
     return [token_iob[label] for token_iob in sent_iob]
+
+
+def pred_to_iob(pred, filenames, true_iob_dir, pred_iob_dir):
+    """
+    Convert predictions from CRF classifier to IOB tags
+
+    Parameters
+    ----------
+    pred: prediction from CRF classifier
+    filenames: filename origin for each sentence
+    true_iob_dir: directory for annotated IOB tags
+    pred_iob_dir: directory for predicted IOB tags
+    """
+    def write_pred_iob():
+        pred_iob_fname = join(pred_iob_dir, prev_iob_fname)
+        with open(pred_iob_fname, 'w') as outf:
+            print('writing ' + pred_iob_fname)
+            json.dump(true_iob, outf, indent=4, sort_keys=True)
+
+    makedirs(pred_iob_dir, exist_ok=True)
+    prev_iob_fname = None
+
+    for sent_count, iob_fname in enumerate(filenames):
+        if iob_fname != prev_iob_fname:
+            if prev_iob_fname:
+                write_pred_iob()
+            true_iob_fname = join(true_iob_dir, iob_fname)
+            true_iob = json.load(open(true_iob_fname))
+            true_iob_iter = iter(true_iob)
+            prev_iob_fname = iob_fname
+
+        sent_iob = next(true_iob_iter)
+
+        for token_count, token_iob in enumerate(sent_iob):
+            for ent in ENTITIES:
+                token_iob[ent] = pred[ent][sent_count][token_count]
+
+    write_pred_iob()
