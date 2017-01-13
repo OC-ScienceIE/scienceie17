f7a998b3401815967647111f0250de5a36dd42b6
Erwin Marsi <emarsi@idi.ntnu.no>
2017-01-13 21:58:47 +0100

    adapted wordfeats exp to new framework

--- exps/wordfeats/wordfeats1.py
+++ exps/wordfeats/wordfeats1.py
@@ -1,22 +1,15 @@
 """
 Extension of simple CRF exp from crf1-cv-exp.py
-with a number of word features
+with pruning
+with word features
 """
-
 
 from os.path import join
 
-from sklearn_crfsuite import CRF
-from sklearn_crfsuite.metrics import flat_classification_report
-
-from sklearn.model_selection import cross_val_predict, GroupKFold
-
-from sie import ENTITIES, LOCAL_DIR, DATA_DIR, EXPS_DIR
-from sie.crf import collect_crf_data, pred_to_iob
+from sie.crf import PruneCRF
+from sie import ENTITIES, LOCAL_DIR, EXPS_DIR
 from sie.feats import generate_feats, features1
-from sie.brat import iob_to_brat
-
-from eval import calculateMeasures
+from sie.exp import run_exp_train_cv, eval_exp_train
 
 
 def wordfeats1(sent):
@@ -40,7 +33,9 @@ def wordfeats1(sent):
             'suffix3': token.orth_[-3:] if l > 2 else '',
             'prefix4': token.orth_[:4] if l > 3 else '',
             'suffix4': token.orth_[-4:] if l > 3 else '',
-            'char_size': l
+            'char_size': l,
+            'is_stop': token.is_stop,
+            'rank': token.rank
         }
 
         sent_feats.append(token_feats)
@@ -55,49 +50,19 @@ word_feats_dir = join('_train', 'wordfeats1')
 
 generate_feats(spacy_dir, word_feats_dir, wordfeats1)
 
-# Step 2: Collect data for running CRF classifier
+
+# Step 2: Run experiments
+
+crf = PruneCRF(c1=0.1, c2=0.1, all_possible_transitions=True)
 
 base_feats_dir = join(EXPS_DIR, 'crf1/_train/features1')
-true_iob_dir = join(LOCAL_DIR, 'train', 'iob')
+feat_dirs = [base_feats_dir, word_feats_dir]
+preds = {}
 
-data = collect_crf_data(true_iob_dir, base_feats_dir, word_feats_dir)
-
-# Step 3: Create folds
-
-# create folds from complete texts only (i.e. instances of the same text
-# are never in different folds)
-# TODO How to set seed for random generator?
-#group_k_fold = GroupKFold(n_splits=5)
-group_k_fold = GroupKFold(n_splits=3)
-
-# use same split for all three entities
-splits = list(
-    group_k_fold.split(data['feats'], data['Material'], data['filenames']))
-
-# Step 4: Run CRF classifier
-crf = CRF(c1=0.1, c2=0.1, all_possible_transitions=True)
-pred = {}
-
-for ent in ENTITIES:
-    pred[ent] = cross_val_predict(crf, data['feats'], data[ent], cv=splits)
-    # Report scores directly on I and B tags,
-    # disregard 'O' because it is by far the most frequent class
-    print('\n' + ent + ':\n')
-    print(flat_classification_report(data[ent], pred[ent], digits=3,
-                                     labels=('B', 'I')))
+for label in ENTITIES:
+    preds[label] = run_exp_train_cv(crf, feat_dirs, label)
 
 
-# Step 5: Convert CRF prediction to IOB tags
-pred_iob_dir = '_train/iob'
+# Step 3: Evaluate
 
-pred_to_iob(pred, data['filenames'], true_iob_dir, pred_iob_dir)
-
-# Step 6: Convert predicted IOB tags to predicted Brat annotations
-txt_dir = join(DATA_DIR, 'train')
-brat_dir = '_train/brat'
-
-iob_to_brat(pred_iob_dir, txt_dir, brat_dir)
-
-# Step 7: Evaluate
-calculateMeasures(txt_dir, brat_dir, 'rel')
-
+eval_exp_train(preds)
